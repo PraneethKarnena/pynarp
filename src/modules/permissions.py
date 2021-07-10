0b5a7e31b4c39d58e5d66aa980b6f4c594d05a05
Praneeth Karnena <praneeth.codes@gmail.com>
2021-07-11 00:38:56 +0530

    Remove dead code

--- src/modules/permissions.py
+++ src/modules/permissions.py
@@ -4,9 +4,3 @@ import os
 
 def has_permissions():
     return os.geteuid() == 0
-    # try:
-    #     os.rename('/etc/foo', '/etc/bar')
-
-    # except IOError as e:
-    #     if (e[0] == errno.EPERM):
-    #         sys.exit("You need root permissions to do this, laterz!")
