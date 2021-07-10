2e4eed153e70848211a9e07ffbf509633712d5fe
Praneeth Karnena <praneeth.codes@gmail.com>
2021-07-10 17:22:01 +0530

    Remove dead code

--- pynarp.py
+++ pynarp.py
@@ -1,27 +1,10 @@
-import os
-import sys
-
 from rich.console import Console
 from rich.markdown import Markdown
 
 
-def resource_path(relative_path):
-    """ Get absolute path to resource for PyInstaller """
-
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-        
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
-
 def print_intro():
     file = 'README.md'
-    path = resource_path(file)
-    with open(path) as readme:
+    with open(file) as readme:
         markdown = Markdown(readme.read())
 
     console = Console()
